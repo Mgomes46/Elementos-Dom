fd98d0e77b525954a98fd78615a54528a76105ad
Mgomes46 <mooisesgom@gmail.com>
2024-04-04 16:36:39 -0300

    comit aula 5

--- script.js
+++ script.js
@@ -1,253 +1,117 @@
 
-:root {
-    --color-primary: #123456;
-    --color-secondary: #FFF;
-    --azul-royal: #144480;
-    --fundo-card-timer: rgba(20, 68, 128, 0.30);
-    --lils: #B872FF;
-    --linear-botes: linear-gradient(180deg, #B872FF 0%, #144480 100%);
-}
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-butto-icon") 
+const tempoNaTela = document.querySelector('#timer')
 
-[data-contexto="foco"] {
-    --main-bg-color: linear-gradient(180deg, #8B1FF8 0%, #041832 48.44%, #01080E 100%);
-}
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
-[data-contexto="descanso-curto"] {
-    --main-bg-color: linear-gradient(180deg, #0F725C 0%, #041832 48.44%, #01080E 100%);
-}
+let tempoDecorridoEmSegundos = 1500
+let intervaloId = null
 
-[data-contexto="descanso-longo"] {
-    --main-bg-color: linear-gradient(180deg, #1875E9 0%, #041832 48.44%, #01080E 100%);
-}
+musica.loop = true
 
-* {
-    box-sizing: border-box;
-    max-width: 100%;
-}
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
-html {
-    background: var(--main-bg-color);
-    min-height: 100vh;
-    font-size: 10px;
-}
+focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500
+    alterarContexto('foco')
+    focoBt.classList.add('active')
+})
 
-body {
-    min-height: 100vh;
-    background: url("/imagens/pattern.png");
-    background-size: cover;
-    background-repeat: no-repeat;
-}
+curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
+    alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
+})
 
-h1, h2, h3, h4, h5, h6 {
-    font-family: 'Unbounded', cursive;
-}
+longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
+    alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
+})
 
-.app__logo-figure {
-    display: flex;
-    justify-content: center;
-    padding: 4rem 0;
-}
-
-.app__section-banner-container {
-    width: 99rem;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 4rem;
-    flex-wrap: wrap;
-}
-
-.app__title {
-    line-height: 150%;
-    font-size: 5.2rem;
-    font-style: normal;
-    color: var(--color-secondary);
-    font-weight: 200;
-    max-width: 46rem;
-}
-
-.app__title-strong {
-    font-weight: 600;
-}
-
-.app__section-card-container {
-    display: flex;
-    justify-content: center;
-}
-
-.app__card {
-    border-radius: 3.2rem;
-    border: 0.2rem solid var(--azul-royal);
-    background: var(--fundo-card-timer);
-    padding: 2.4rem;
-    margin: 8rem 0;
-    width: 59rem;
-    max-width: 90%;
-}
-
-.app__card-list {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 1.6rem;
-}
-
-.app__card-button {
-    font-family: Montserrat;
-    font-size: 1.8rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 150%;
-    background: none;
-    padding: 0.8rem;
-    color: var(--color-secondary);
-    border: none;
-    cursor: pointer;
-    outline: inherit;
-}
-
-.app__card-button.active {
-    border-radius: 0.8rem;
-    background: var(--azul-royal);
-    font-weight: 600;
-}
-
-.app__card-timer {
-    color: var(--color-secondary);
-    text-align: center;
-    font-family: Unbounded;
-    font-size: 12rem;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-    margin: 3.2rem;
-    text-align: center;
-}
-
-.app__card-list-label {
-    color: var(--color-secondary);
-    font-family: Montserrat;
-    font-size: 1.8rem;
-    font-style: normal;
-    font-weight: 400;
-}
-
-.app__card-primary-button-wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.app__card-primary-button {
-    padding: 0.8rem;
-    width: 12rem;
-    color: var(--color-secondary);
-    border: none;
-    cursor: pointer;
-    outline: inherit;
-    border-radius: 3.2rem;
-    background: var(--linear-botes);
-    margin-top: 3.2rem;
-    font-size: 1.8rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 1rem;
-}
-
-.app__footer {
-    display: flex;
-    width: 100%;
-    padding: 2.8rem 6rem;
-    justify-content: center;
-    align-items: center;
-    background: #010B15;
-}
-
-.app__footer-text {
-    color: rgba(182, 182, 182, 0.8);
-    text-align: center;
-    font-family: Montserrat;
-    font-size: 1.5rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 150%;
-}
-
-/* toggle */
-.toggle {
-    cursor: pointer;
-    display: inline-block;
-}
-
-.toggle-switch {
-    display: inline-block;
-    background: #ccc;
-    border-radius: 1.6rem;
-    width: 5.8rem;
-    height: 3.2rem;
-    position: relative;
-    vertical-align: middle;
-    transition: background 0.25s;
-}
-
-.toggle-switch:before, .toggle-switch:after {
-    content: "";
-}
-
-.toggle-switch:before {
-    display: block;
-    background: linear-gradient(to bottom, #fff 0%, #eee 100%);
-    border-radius: 50%;
-    width: 2.4rem;
-    height: 2.4rem;
-    position: absolute;
-    top: 0.4rem;
-    left: 0.4rem;
-    transition: left 0.25s;
-}
-
-.toggle:hover .toggle-switch:before {
-    background: linear-gradient(to bottom, #fff 0%, #fff 100%);
-}
-
-.toggle-checkbox:checked + .toggle-switch {
-    background: var(--lils);
-}
-
-.toggle-checkbox:checked + .toggle-switch:before {
-    left: 3rem;
-}
-
-.toggle-checkbox {
-    position: absolute;
-    visibility: hidden;
-}
-
-/* fim - toggle */
-
-/* Para Tablets */
-@media (max-width: 1024px) and (min-width: 768px) {
-    .app__section-banner-container {
-        width: 48rem;
+function alterarContexto(contexto) {
+    mostrarTempo()
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            ` 
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        default:
+            break;
     }
 }
 
-/* Para Celulares */
-@media (max-width: 767px) {
-    .app__section-banner-container {
-        width: 32rem;
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        audioTempoFinalizado.play()
+        alert('Tempo finalizado!')
+        zerar()
+        return
     }
-
-    .app__title {
-        font-size: 2.6rem;
-    }
-
-    .app__card-timer {
-        font-size: 6rem;
-    }
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
 }
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        audioPausa.play()
+        zerar()
+        return
+    }
+    audioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarOuPausarBt.textContent = "Pausar"
+    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/pause.png`)
+}
+
+function zerar() {
+    clearInterval(intervaloId) 
+    iniciarOuPausarBt.textContent = "Começar"
+    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/play_arrow.png`)
+    intervaloId = null
+}
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo()
